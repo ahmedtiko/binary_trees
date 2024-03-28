6f47e089496a4ed24b345cee9e49753f70634605
ahmedtiko <ahmedmmtarek@gmail.com>
2024-03-28 06:02:29 -0700

    Auto Push

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,7 +12,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	left_size = binary_tree_size(tree->left);
 	right_size = binary_tree_size(tree->right);
 
